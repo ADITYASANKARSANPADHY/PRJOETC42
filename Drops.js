96ae048f70763a540be1051fd20d5a3f80f32ad3
ADITYASANKARSANPADHY <76484698+ADITYASANKARSANPADHY@users.noreply.github.com>
2021-06-05 23:47:30 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,17 @@
+class RainDrops {
+    constructor(x,y){
+        var options = {
+            isStatic: false,
+
+        }
+
+
+    }
+/* display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        //display the image for BestMan if the frameCount is over 200, otherwise display the boy with umbrella image
+        image(this.image,pos.x,pos.y+70,300,300);
+    }*/
+
+}
